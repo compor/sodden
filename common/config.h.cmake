1d488e137c41c512e924c758f3d0ab07c40decf8
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2017-11-25 17:25:29 +0000

    update timing capabilities to detect POSIX systems

---
+++ common/config.h.cmake
@@ -0,0 +1,4 @@
+
+/* Define to 1 if you have <unistd.h> */
+#cmakedefine HAVE_UNISTD_H 1
+
--- common/timing.h
+++ common/timing.h
@@ -1,9 +1,16 @@
-/**/
+/*
+ * Common harness timing routines
+ */
 
+#include "common/config.h"
+
+#ifdef HAVE_UNISTD_H
 #include <unistd.h>
 #include <time.h>
+#endif /* HAVE_UNISTD_H */
 
-#if (_POSIX_TIMERS > 0) && defined(_POSIX_MONOTONIC_CLOCK)
+#if defined(HAVE_UNISTD_H) && (_POSIX_TIMERS > 0) &&                           \
+    defined(_POSIX_MONOTONIC_CLOCK)
 #define OLDEN_TIME(x) clock_gettime(CLOCK_MONOTONIC_RAW, &x)
 #define OLDEN_DURATION_MS(start, stop)                                         \
   ((double)stop.tv_sec - start.tv_sec) * 1000 +                                \
