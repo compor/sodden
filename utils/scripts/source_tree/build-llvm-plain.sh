a3365056550d230bfe8ffca9e207035b711b03c1
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2020-01-27 14:45:43 +0000

    Update build script

--- utils/scripts/source_tree/build-llvm-plain.sh
+++ utils/scripts/source_tree/build-llvm-plain.sh
@@ -8,18 +8,27 @@ BMK_CONFIG_FILE="${SRC_DIR}/config/suite_all.txt"
 
 #
 
-C_FLAGS="-g -Wall"
+C_FLAGS=""
+C_FLAGS="${C_FLAGS} -g -Wall"
 C_FLAGS="${C_FLAGS} -O2"
+C_FLAGS="${C_FLAGS} -fsave-optimization-record"
+C_FLAGS="${C_FLAGS} -fdiagnostics-show-hotness"
 C_FLAGS="${C_FLAGS} -D__STRICT_ANSI__"
 
+CXX_FLAGS=""
 CXX_FLAGS="${C_FLAGS}"
+
+# C-only flags
+
+C_FLAGS="${C_FLAGS} -std=c99"
 
 #LINKER_FLAGS="-Wl,-L$(llvm-config --libdir) -Wl,-rpath=$(llvm-config --libdir)"
 #LINKER_FLAGS="${LINKER_FLAGS} -lc++ -lc++abi" 
 
-CC=clang CXX=clang++ \
 cmake \
   -GNinja \
+  -DCMAKE_C_COMPILER=clang \
+  -DCMAKE_CXX_COMPILER=clang++ \
   -DCMAKE_POLICY_DEFAULT_CMP0056=NEW \
   -DCMAKE_EXPORT_COMPILE_COMMANDS=On \
   -DCMAKE_BUILD_TYPE=Debug \
@@ -29,6 +38,6 @@ cmake \
   -DCMAKE_SHARED_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_MODULE_LINKER_FLAGS="${LINKER_FLAGS}" \
   -DCMAKE_INSTALL_PREFIX="${INSTALL_PREFIX}" \
-  -DHARNESS_BMK_CONFIG_FILE=${BMK_CONFIG_FILE} \
+  -DHARNESS_BMK_CONFIG_FILE="${BMK_CONFIG_FILE}" \
   "${SRC_DIR}"
 
