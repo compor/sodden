190f9de31341205c736c0c01c19ada34009d8712
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2020-01-27 14:45:22 +0000

    Add test feature macro for X/OPEN

--- common/timing.h
+++ common/timing.h
@@ -5,6 +5,13 @@
 #include "common/config.h"
 
 #ifdef HAVE_UNISTD_H
+
+#if __STDC_VERSION__ >= 199901L
+#define _XOPEN_SOURCE 600
+#else
+#define _XOPEN_SOURCE 500
+#endif /* __STDC_VERSION__ */
+
 #include <unistd.h>
 #include <time.h>
 #endif /* HAVE_UNISTD_H */
